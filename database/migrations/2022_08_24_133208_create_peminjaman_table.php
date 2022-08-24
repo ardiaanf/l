9ad69b22421d82999812752cff6242d59777e119
ardiaanf <ardiaanf@gmail.com>
2022-08-24 20:51:41 +0700

    fix

---
+++ database/migrations/2022_08_24_133208_create_peminjaman_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_anggota');
+            $table->integer('id_buku');
+            $table->date('tgl_pinjam');
+            $table->date('tgl_kembali');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
